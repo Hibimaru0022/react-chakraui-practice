9d5d5b8a1939344f7a0d5278c805f0357eed2446
Hibimaru0022 <sherbet02@icloud.com>
2022-05-15 21:58:03 +0900

    setting route

---
+++ src/router/Router.tsx
@@ -0,0 +1,30 @@
+import { FC, memo } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Login } from '../components/pages/Login';
+import { Page404 } from '../components/pages/Page404';
+import { homeRoutes } from './HomeRoute';
+
+export const Router: FC = memo(() => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
